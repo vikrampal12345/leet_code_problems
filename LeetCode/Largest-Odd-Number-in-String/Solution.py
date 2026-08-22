0b4b8773280b8448ae8b4f4da273f3a01892163d
Vikram pal <vikrampal12345ram@gmail.com>
2026-08-22 15:38:38 +0530

    Add solution for Largest Odd Number in String

--- LeetCode/Largest-Odd-Number-in-String/Solution.py
+++
@@ -1,7 +0,0 @@
-1class Solution:
-2    def largestOddNumber(self, num: str) -> str:
-3        for i in range(len(num)-1, -1, -1):
-4            if int(num[i])%2 != 0:
-5                return num[:i+1]
-6        return ""         
-7         
